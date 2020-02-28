11cf363311c881622c3c2f5d3c5517ae4899e509
Gabriel Hephzibah <gabrielhephzibah24@gmail.com>
2020-02-28 10:45:44 +0100

    Initial commit to android template

---
+++ app/src/main/java/com/enyata/framework/mvvm/ViewModelProviderFactory.java
@@ -0,0 +1,43 @@
+package com.enyata.framework.mvvm;
+
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.enyata.framework.mvvm.data.DataManager;
+import com.enyata.framework.mvvm.ui.mainActivity.MainActivityViewModel;
+import com.enyata.framework.mvvm.ui.splash.SplashViewModel;
+import com.enyata.framework.mvvm.utils.rx.SchedulerProvider;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+/**
+ * Created by jyotidubey on 22/02/19.
+ */
+@Singleton
+public class ViewModelProviderFactory extends ViewModelProvider.NewInstanceFactory {
+
+  private final DataManager dataManager;
+  private final SchedulerProvider schedulerProvider;
+
+  @Inject
+  public ViewModelProviderFactory(DataManager dataManager,
+      SchedulerProvider schedulerProvider) {
+    this.dataManager = dataManager;
+    this.schedulerProvider = schedulerProvider;
+  }
+
+
+  @Override
+  public <T extends ViewModel> T create(Class<T> modelClass) {
+    if (modelClass.isAssignableFrom(MainActivityViewModel.class)) {
+      //noinspection unchecked
+      return (T) new MainActivityViewModel(dataManager,schedulerProvider);
+    }
+    else if (modelClass.isAssignableFrom(SplashViewModel.class)) {
+      //noinspection unchecked
+      return (T) new SplashViewModel(dataManager,schedulerProvider);
+    }
+    throw new IllegalArgumentException("Unknown ViewModel class: " + modelClass.getName());
+  }
+}
